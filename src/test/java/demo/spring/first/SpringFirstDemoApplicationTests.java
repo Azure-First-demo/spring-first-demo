58c11056fca31f02bf1c0df42f23d155185d7ec3
Sagar gupta <sagargupta0761@gmail.com>
2022-05-15 20:15:49 +0530

    test

--- src/test/java/demo/spring/first/SpringFirstDemoApplicationTests.java
+++ src/test/java/demo/spring/first/SpringFirstDemoApplicationTests.java
@@ -4,16 +4,25 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+
+@WebMvcTest(SpringFirstDemoApplication.class)
 class SpringFirstDemoApplicationTests {
 	@Value("${server.port}")
 	String port;
 
 	@Autowired
-	SpringFirstDemoApplication springFirstDemoApplication;
+	private MockMvc mockMvc;
 
 	@Autowired
 	RestTemplate restTemplate;
@@ -21,8 +30,9 @@ class SpringFirstDemoApplicationTests {
 
 
 	@Test
-	void contextLoads() {
-		Assertions.assertEquals("Hello world",springFirstDemoApplication.defaultPage());
+	void contextLoads() throws Exception {
+		this.mockMvc.perform(get("/")).andExpect(status().isOk())
+				.andExpect(content().string(containsString("Hello world")));
 	}
 
 }
